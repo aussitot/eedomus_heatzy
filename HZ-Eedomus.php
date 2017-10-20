88d2f4ce11a0a47f158af174cfedf497429747e4
aussitot <contact@aussitot.net>
2017-10-20 16:48:45 +0200

    V0.4
    Récupération de l’état actuel du radiateur

--- HZ-Eedomus.php
+++ HZ-Eedomus.php
@@ -9,8 +9,16 @@ script cree par twitter:@Havok pour la eedomus
 mode = did - Récupération des identifiants did des appareils
 */
 
+//--- Paramètres Heatzy
+$heatzy_username = "";
+$heatzy_password = "";
+$did = '';
+
+//--- Paramètres Script
+$debug = false;
+$heatzy_application_id = "c70a66ff039d41b4a220e198b0fcc8b3";
+
 define('__ROOT__', dirname(dirname(__FILE__)));
-require_once ('config.php');
 
 //-------------- Parametres
 $mode = $_GET['mode'];
@@ -73,10 +81,49 @@ function heatzy_bindings($token, $appid) {
   //return $obj['devices'][0]['did'];
 }
 
+//*************   Récupération de l'etat de l'appareil
+// GET /app/devdata/{did}/latest
+function heatzy_getstatus($diditem, $appid) {
+
+  $curl = curl_init(); //Première étape, initialiser une nouvelle session cURL.
+  curl_setopt($curl, CURLOPT_URL, 'https://euapi.gizwits.com/app/devdata/'.$diditem.'/latest'); //Il va par exemple falloir lui fournir l'url de la page à récupérer.
+  curl_setopt($curl, CURLOPT_HTTPGET, true); //Pour envoyer une requête POST, il va alors tout d'abord dire à la fonction de faire un HTTP POST
+  curl_setopt ($curl, CURLOPT_HTTPHEADER, array("Accept: application/json","X-Gizwits-Application-Id: ".$appid));
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); //Cette option permet d'indiquer que nous voulons recevoir le résultat du transfert au lieu de l'afficher.
+
+  $return = curl_exec($curl); //Il suffit ensuite d'exécuter la requête
+  curl_close($curl);
+
+  $obj = json_decode($return,true);
+  $etatch = $obj['attr']['mode'];
+  switch ($etatch) {
+    case '舒适':
+      $etat = 'confort';
+      break;
+    case '停止':
+      $etat = 'off';
+      break;
+    case '经济':
+      $etat = 'eco';
+      break;
+    case '解冻':
+      $etat = 'hors-gel';
+      break;
+    default:
+      $etat = 'inconnu';
+      break;
+  }
+  return $etat;
+  //return $obj['devices'][0]['did'];
+}
+
 if ($mode == 'did') {
   /* récupération du token */
   $token = heatzy_login($heatzy_username,$heatzy_password,$heatzy_application_id );
   $did = heatzy_bindings($token,$heatzy_application_id);
   echo $did;
+} else {
+  $result = heatzy_getstatus($did,$heatzy_application_id);
+  echo $result;
 }
  ?>
