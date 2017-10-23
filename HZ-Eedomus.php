cbc1ace1b4f02dac0eeec9e8b7ce42249cf222ca
aussitot <contact@aussitot.net>
2017-10-23 15:53:36 +0200

    V1.0
    Première version fonctionnelle

--- HZ-Eedomus.php
+++ HZ-Eedomus.php
@@ -7,25 +7,35 @@ script cree par twitter:@Havok pour la eedomus
 
 ------------------- Parametres :
 mode = did - Récupération des identifiants did des appareils
+mode = set - Modification de l'etat de l'appareil
+  ordre = 0 confort
+  ordre = 1 eco
+  ordre = 2 hors-gel
+  ordre = 3 off
 */
 
 //--- Paramètres Heatzy
 $heatzy_username = "";
 $heatzy_password = "";
-$did = '';
+//$did = '';
 
 //--- Paramètres Script
 $debug = false;
 $heatzy_application_id = "c70a66ff039d41b4a220e198b0fcc8b3";
 
+
 define('__ROOT__', dirname(dirname(__FILE__)));
 
 //-------------- Parametres
-$mode = $_GET['mode'];
+if (isset($_GET['mode'])) $mode = $_GET['mode'];
+if (isset($_GET['ordre'])) $ordre = $_GET['ordre'];
+if (isset($_GET['did'])) $did = $_GET['did'];
 
 //*************   Récupération du token, validité 7 jours
 // POST /app/login (User login)
 function heatzy_login($login, $password, $appid) {
+
+  global $debug;
 
   $curl = curl_init(); //Première étape, initialiser une nouvelle session cURL.
   curl_setopt($curl, CURLOPT_URL, 'https://euapi.gizwits.com/app/login'); //Il va par exemple falloir lui fournir l'url de la page à récupérer.
@@ -43,14 +53,19 @@ function heatzy_login($login, $password, $appid) {
 
   $return = curl_exec($curl); //Il suffit ensuite d'exécuter la requête
   curl_close($curl);
-
+/*
   $obj = json_decode($return);
-  return $obj->{'token'}; // 12345
+  return $obj->{'token'}; // 12345*/
+  $obj = json_decode($return,true);
+  if ($debug==true) { error_log(date("d-m-Y H:i:s").' heatzy_login: '.print_r($obj,true)."\n", 3, "HZ.log"); }
+  return $obj['token'];
 }
 
 //*************   Récupération did (identifiant de l'appareil (validité permanente))
 // GET /app/bindings
 function heatzy_bindings($token, $appid) {
+
+  global $debug;
 
   $curl = curl_init(); //Première étape, initialiser une nouvelle session cURL.
   curl_setopt($curl, CURLOPT_URL, 'https://euapi.gizwits.com/app/bindings?limit=20&skip=0'); //Il va par exemple falloir lui fournir l'url de la page à récupérer.
@@ -62,6 +77,7 @@ function heatzy_bindings($token, $appid) {
   curl_close($curl);
 
   $obj = json_decode($return,true);
+  if ($debug==true) { error_log(date("d-m-Y H:i:s").' heatzy_bindings: '.print_r($obj,true)."\n", 3, "HZ.log"); }
 
   reset($obj['devices']);
   $result = "<table border='1'><t<thead><tr><th>N°</th><th>Nom</th><th>@Mac</th><th>did</th></tr></thead><tbody>";
@@ -85,6 +101,8 @@ function heatzy_bindings($token, $appid) {
 // GET /app/devdata/{did}/latest
 function heatzy_getstatus($diditem, $appid) {
 
+  global $debug;
+
   $curl = curl_init(); //Première étape, initialiser une nouvelle session cURL.
   curl_setopt($curl, CURLOPT_URL, 'https://euapi.gizwits.com/app/devdata/'.$diditem.'/latest'); //Il va par exemple falloir lui fournir l'url de la page à récupérer.
   curl_setopt($curl, CURLOPT_HTTPGET, true); //Pour envoyer une requête POST, il va alors tout d'abord dire à la fonction de faire un HTTP POST
@@ -95,35 +113,97 @@ function heatzy_getstatus($diditem, $appid) {
   curl_close($curl);
 
   $obj = json_decode($return,true);
+  if ($debug==true) { error_log(date("d-m-Y H:i:s").' heatzy_getstatus: '.print_r($obj,true)."\n", 3, "HZ.log"); }
+
   $etatch = $obj['attr']['mode'];
   switch ($etatch) {
     case '舒适':
-      $etat = 'confort';
+      $etat = 0; // 'confort';
       break;
     case '停止':
-      $etat = 'off';
+      $etat = 3; //'off';
       break;
     case '经济':
-      $etat = 'eco';
+      $etat = 1; //'eco';
       break;
     case '解冻':
-      $etat = 'hors-gel';
+      $etat = 2; //'hors-gel';
       break;
     default:
-      $etat = 'inconnu';
+      $etat = 99; //'inconnu';
       break;
   }
   return $etat;
   //return $obj['devices'][0]['did'];
 }
 
-if ($mode == 'did') {
-  /* récupération du token */
-  $token = heatzy_login($heatzy_username,$heatzy_password,$heatzy_application_id );
-  $did = heatzy_bindings($token,$heatzy_application_id);
+//*************   Mise en forme xml du status pour la eedomus
+function heatzy_eedomusstatus($status) {
+
+  $eestatus = "<root><status>";
+  switch ($status) {
+    case '0':
+      $eestatus .= '<libelle>confort</libelle><value>0</value>';
+      break;
+    case '1':
+      $eestatus .= '<libelle>eco</libelle><value>1</value>';
+      break;
+    case '2':
+      $eestatus .= '<libelle>hors-gel</libelle><value>2</value>';
+      break;
+    case '3':
+      $eestatus .= '<libelle>off</libelle><value>3</value>';
+      break;
+
+    default:
+      # code...
+      break;
+  }
+  $eestatus .= "</status></root>";
+  return $eestatus;
+}
+
+//*************   Modification de l'etat de l'appareil
+// POST /app/control/{did}
+function heatzy_setstatus($diditem, $appid, $token, $ordre) {
+
+  global $debug;
+
+  if ($ordre !=0 && $ordre !=1 && $ordre !=2 && $ordre !=3) {
+    return 'code ordre incorrect';
+  }
+  $ordreapi = array(1,1,$ordre+0);
+
+  $curl = curl_init(); //Première étape, initialiser une nouvelle session cURL.
+  curl_setopt($curl, CURLOPT_URL, 'https://euapi.gizwits.com/app/control/'.$diditem); //Il va par exemple falloir lui fournir l'url de la page à récupérer.
+  curl_setopt($curl, CURLOPT_POST, true); //Pour envoyer une requête POST, il va alors tout d'abord dire à la fonction de faire un HTTP POST
+  curl_setopt ($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json","Accept: application/json","X-Gizwits-Application-Id: ".$appid,"X-Gizwits-User-token: ".$token));
+
+  $postfields = array(
+    'raw' => $ordreapi
+  );
+  $postfields = json_encode($postfields);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); //Cette option permet d'indiquer que nous voulons recevoir le résultat du transfert au lieu de l'afficher.
+
+  $return = curl_exec($curl); //Il suffit ensuite d'exécuter la requête
+  curl_close($curl);
+
+  $obj = json_decode($return,true);
+  if ($debug==true) { error_log(date("d-m-Y H:i:s").' heatzy_setstatus: '.print_r($obj,true)."\n", 3, "HZ.log"); }
+
+  return $obj;
+}
+
+if (isset($mode) && $mode == 'did') {
+  $token = heatzy_login($heatzy_username,$heatzy_password,$heatzy_application_id ); // récupération du token
+  $did = heatzy_bindings($token,$heatzy_application_id); // récupération du did
   echo $did;
+} elseif (isset($mode) && $mode == 'set') {
+  $token = heatzy_login($heatzy_username,$heatzy_password,$heatzy_application_id ); // récupération du token
+  $result = heatzy_setstatus($did,$heatzy_application_id,$token,$ordre); // définition du status actuel
 } else {
-  $result = heatzy_getstatus($did,$heatzy_application_id);
-  echo $result;
+  $status = heatzy_getstatus($did,$heatzy_application_id); //récupération du status actuel
+  echo heatzy_eedomusstatus($status); //mise en forme du xml pour eedomus
 }
  ?>
--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # eedomus_heatzy
 Gestion des modules Heatzy dans la eedomus
+script cree par twitter:@Havok pour la eedomus
+
+NB : Script à installer sur un serveur web/php autre que l'eedomus elle-même
+
+# INSTALLATION
+Bonjour,
+
+Voici un  script pour intégrer dans l'interface eedomus les chauffages électriques pilotés par des modules Heatzy
+A notez que ce script php peut être utilisé pour autre chose que la eedomus.
+
+**Prérequis** : Il faut disposer d'un serveur web/php autre que l'eedomus elle-même.  
+
+**Ce que ca fait** : Ca va vous permettre de
+- changer l'etat des radiateurs
+- mettre à jour régulierement l'etat de ceux-ci dans l'interface eedomus (en cas de changements programmés dans l'appli Heatzy)
+
+## Etape 1
+- Copiez le fichier HZ-eedomus.php sur votre serveur.
+- Modifiez le pour y reporter vos login et password Heatzy
+
+```php
+//--- Paramètres Heatzy
+$heatzy_username = "";
+$heatzy_password = "";
+```
+## Etape 2
+Dans un navigateur tapez l'url : ```http://www.votreserveur.com/HZ-Eedomus.php?mode=did``` et récupérer la valeur **did** correspondant au radiateur que vous souhaitez piloter dans l'eedomus
+
+## Etape 3
+Dans eedomus créez un Actionneur http :
+- Nom : Chauffage Heatzy (ou ce que vous voulez ;)
+- [VAR1] : le **did** récupéré à l'étape 2
+
+Dans les paramètres experts :
+- requete de mise à jour : ```http://www.votreserveur.com/HZ-Eedomus.php?did=[VAR1]```
+- Chemin XPath : /root/status/value
+- Fréquence de la requête : en mn le délai que vous souhaitez pour l'état (30mn me semble pas mal).
+
+Dans l'onglet Valeurs :
+- Valeur brute : 0
+- description : Confort
+- URL : ```http://www.votreserveur.com/HZ-Eedomus.php?mode=set&ordre=0&did=[VAR1]```
+- Type : GET
+
+- Valeur brute : 1
+- description : Eco
+- URL : ```http://www.votreserveur.com/HZ-Eedomus.php?mode=set&ordre=1&did=[VAR1]```
+- Type : GET
+
+- Valeur brute : 2 
+- description : Hors-Gel
+- URL : ```http://www.votreserveur.com/HZ-Eedomus.php?mode=set&ordre=2&did=[VAR1]```
+- Type : GET
+
+- Valeur brute : 3
+- description : Off
+- URL : ```http://www.votreserveur.com/HZ-Eedomus.php?mode=set&ordre=3&did=[VAR1]```
+- Type : GET
+
+Et c'est tout.
+Si j'ai le courage je tenterais un script intégré au Store eedomus.
+Bonne chance (Anne-marie tu peux le faire !)
